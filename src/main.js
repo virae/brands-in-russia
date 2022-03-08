fd736e70c959003515ab44a1c6c70225dd468745
Michal Šimonfy <michal.simonfy@gmail.com>
2022-03-08 11:43:51 +0100

    Add intersection observer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import './assets/tailwind.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+import VueObserveVisibility from 'vue-observe-visibility'
+app.use(VueObserveVisibility)
+app.mount('#app')
