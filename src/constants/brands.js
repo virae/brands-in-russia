1ed561fe1ea89ec6761f18612ba648b3871636c9
Michal Šimonfy <michal.simonfy@gmail.com>
2022-03-08 11:44:04 +0100

    Configure brands

---
+++ src/constants/brands.js
@@ -0,0 +1,622 @@
+export default [
+  {
+    id: 'coca-cola',
+    name: 'Coca Cola',
+    description: '',
+    status: -1,
+    twitter: 'cocacola',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'mcdonalds',
+    name: 'McDonald\'s',
+    description: '',
+    status: -1,
+    twitter: 'mcdonalds',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'kfc',
+    name: 'KFC',
+    description: '',
+    status: -1,
+    twitter: 'kfc',
+    facebook: ''
+  },
+  {
+    id: 'unilever',
+    name: 'Unilever',
+    description: '',
+    status: -1,
+    twitter: 'unilever',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'uniqlo',
+    name: 'Uniqlo',
+    description: '',
+    status: -1,
+    twitter: 'uniqlousa',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'represent',
+    name: 'Represent',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'google',
+    name: 'Google',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'adobe',
+    name: 'Adobe',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'nintendo',
+    name: 'Nintendo',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'dropbox',
+    name: 'Dropbox',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'playstation',
+    name: 'Playstation',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'sony-pictures',
+    name: 'Sony pictures',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'instagram',
+    name: 'Instagram',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'samsung',
+    name: 'Samsung',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'youtube',
+    name: 'YouTube',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'twitter',
+    name: 'Twitter',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'hp',
+    name: 'HP',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'intel',
+    name: 'Intel',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'ericsson',
+    name: 'Ericsson',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'lenovo',
+    name: 'Lenovo',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'amd',
+    name: 'AMD',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'nokia',
+    name: 'Nokia',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'dhl',
+    name: 'DHL',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'fedex',
+    name: 'FedEx',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'ups',
+    name: 'UPS',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'maersk',
+    name: 'Maersk',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+
+
+
+
+
+  {
+    id: 'microsoft',
+    name: 'Microsoft',
+    description: 'Microsoft said it was “horrified” by the situation in Ukraine, and would suspend all new sales of its products and services in Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'apple',
+    name: 'Apple',
+    description: 'Apple halted product sales and restricted Apple Pay Russia, disabled Apple Maps in Ukraine, and removed Russian state media outlets from its App Store.',
+    status: 0,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'dell',
+    name: 'Dell',
+    description: 'Dell has suspended product sales.',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'electronic-arts',
+    name: 'Electronic Arts',
+    description: 'Electronic Arts, which makes hit games such as FIFA 22 and Battlefield, has stopped selling content, games, and virtual currency in Russia and Belarus.',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'sainsburys',
+    name: 'Sainsbury’s',
+    description: 'Sainsbury’s said it would remove all products that are 100% sourced from Russia, including Russian Standard vodka.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'mango',
+    name: 'Mango',
+    description: 'Mango, which employs 800 people in Russia, said it would temporarily close its stores and its online platform.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'ikea',
+    name: 'IKEA',
+    description: 'Ikea has closed its 17 stores in Russia, which is the Swedish company\'s 10th largest retail market, and has paused sourcing materials from Russia and Belarus.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'hm',
+    name: 'H&M',
+    description: 'H&M, the world\'s second-largest clothing retailer, has paused all of its sales in Russia, and has also temporarily closed all its Ukraine branches due to safety concerns.',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'burberry',
+    name: 'Burberry',
+    description: 'Burberry has stopped sending products to Russia, but its two flagship stores there remain open for now.',
+    status: 0,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'hermes',
+    name: 'Hermès',
+    description: 'Hermes has temporarily closed its stores in Russia, the first luxury goods giant to do so.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'nike',
+    name: 'Nike',
+    description: 'Nike has stopped taking online orders for Russian customers as processing payments becomes more difficult with the imposition of sanctions, but its stores remain open.',
+    status: 0,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'adidas',
+    name: 'Adidas',
+    description: 'German sportswear brand Adidas has announced the suspension of its partnership with the Russian Football Union. This comes after UEFA banned all Russian football clubs from its competitions',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'netflix',
+    name: 'Netflix',
+    description: 'Netflix has temporarily halted all future film and television projects and acquisitions in Russia, and has defied calls from the government to add state news channels to its service.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'airbnb',
+    name: 'Aribnb',
+    description: 'Airbnb has suspended all operations in Russia and Belarus, hitting Russia’s 90,000 active short-term rentals on the service.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'spotify',
+    name: 'Spotify',
+    description: 'Spotify has “indefinitely” closed its Russian offices and removed state-backed media content from its platform.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'mercedes',
+    name: 'Mercedes Benz',
+    description: 'Mercedes-Benz Group has halted the sale of cars and vans in Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'harley-davidson',
+    name: 'Harley Davidson',
+    description: 'Harley-Davidson has paused the shipment of motorcycles to Russia, saying its thoughts are on the “safety of the people of Ukraine."',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'bmw',
+    name: 'BMW',
+    description: 'BMW has stopped the export of cars to Russia and said it would stop production on the ground there.',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'airbus',
+    name: 'Airbus',
+    description: 'Airbus pledged to cease its support for Russia’s aviation industry, where it maintains aircraft used by the country’s main commercial airlines.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'boeing',
+    name: 'Boeing',
+    description: 'Boeing has said it will suspend parts, maintenance and technical support to Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'mastercard',
+    name: 'MasterCard',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'visa',
+    name: 'Visa',
+    description: 'Visa said on March 5 that it would temporarily halt business in Russia, effective immediately, with all transactions stopping over the course of the next few days. ',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'paypal',
+    name: 'PayPal',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+
+
+  {
+    id: 'disney',
+    name: 'Disney',
+    description: 'Disney became the first entertainment giant to pause theatrical releases in Russia on Monday. ',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'warner-bros',
+    name: 'Warner Bros.',
+    description: 'On Tuesday, Warner Bros. followed suit and announced it would also pause releases in Russia, including "The Batman," NYT reported. ',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'expedia',
+    name: 'Expedia',
+    description: 'Online travel-booking company Expedia ceased the sale of travel into and out of Russia on Wednesday, an Expedia spokesperson confirmed with Insider.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'exxonmobil',
+    name: 'ExxonMobil',
+    description: 'In a statement on Tuesday, ExxonMobil said it would begin the process to discontinue operations and develop steps to exit the Sakhalin-1 venture, a project it operates on behalf of an international consortium of Japanese, Indian, and Russian companies.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'general-motors',
+    name: 'General Motors',
+    description: 'GM is pulling from Russia "due to a number of external factors, including supply chain issues and other matters beyond the company\'s control," a spokesperson said.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'bp',
+    name: 'BP',
+    description: '',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'louis-vuitton',
+    name: 'Louis Vuitton',
+    description: 'Luxury fashion giant LVMH — which owns several high-end brands including Louis Vuitton, Christian Dior, Fendi, and Givenchy — confirmed to The New York Times on March 4 that it will temporarily shutter its stores in Russia "given the current circumstances in the region."',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'christian-dior',
+    name: 'Christian Dior',
+    description: 'Luxury fashion giant LVMH — which owns several high-end brands including Louis Vuitton, Christian Dior, Fendi, and Givenchy — confirmed to The New York Times on March 4 that it will temporarily shutter its stores in Russia "given the current circumstances in the region."',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'givenchy',
+    name: 'Givenchy',
+    description: 'Luxury fashion giant LVMH — which owns several high-end brands including Louis Vuitton, Christian Dior, Fendi, and Givenchy — confirmed to The New York Times on March 4 that it will temporarily shutter its stores in Russia "given the current circumstances in the region."',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'gucci',
+    name: 'Gucci',
+    description: 'Kering, the conglomerate behind luxury brands such as Gucci, Saint Laurent, Bottega Veneta, and Balenciaga, announced in a statement on March 4 that it is temporarily closing all stores in Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'bottega-veneta',
+    name: 'Bottega Veneta',
+    description: 'Kering, the conglomerate behind luxury brands such as Gucci, Saint Laurent, Bottega Veneta, and Balenciaga, announced in a statement on March 4 that it is temporarily closing all stores in Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'balenciaga',
+    name: 'Balenciaga',
+    description: 'Kering, the conglomerate behind luxury brands such as Gucci, Saint Laurent, Bottega Veneta, and Balenciaga, announced in a statement on March 4 that it is temporarily closing all stores in Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'chanel',
+    name: 'Chanel',
+    description: '"We will no longer deliver into Russia, we will close our boutiques, and we already suspended our e-commerce," Chanel wrote in a LinkedIn post on March 4. ',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'zara',
+    name: 'Zara',
+    description: 'Inditex, the largest global fashion company and owner of the fast-fashion brand Zara, told Reuters on March 5 it will temporarily halt all business with Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'american-express',
+    name: 'American Express',
+    description: 'American Express announced on March 6 that globally issued cards will no longer work in Russia, while cards within the country will cease to function within the company\'s larger network. It also suspended business in Belarus.',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'tiktok',
+    name: 'TikTok',
+    description: 'TikTok suspended all livestreaming and content sharing in Russia on March 6, writing in a post on its website that it had  "no choice" but to halt service in response to the country\'s new "fake news" law.',
+    status: 1,
+    twitter: '',
+    facebook: '',
+    small: true
+  },
+  {
+    id: 'puma',
+    name: 'Puma',
+    description: 'Sportswear giant Puma has temporarily closed all of its 100 stores in Russia. A spokesperson for the company confirmed to Insider that all of its employees in Russia will continue to be paid in full.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'prada',
+    name: 'Prada',
+    description: 'Italian luxury brand Prada has temporarily shut down all of its retail operations in Russia.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'pwc',
+    name: 'PwC',
+    description: 'Accountancy giant PwC said in a statement Sunday that it is cutting ties with the Russian arm of its business, where it has 3,700 staff and partners. PwC Russia will now operate as a standalone company under a different name with no official link to PwC. ',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'kpmg',
+    name: 'KPMG',
+    description: 'KPMG, another accountancy giant, announced similar changes to its rival PwC on Sunday. The company said its Russia and Belarus firms will leave the KPMG network, which employs more than 4,500 people.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  {
+    id: 'lego',
+    name: 'Lego',
+    description: 'Lego has stopped shipping toys to 81 stores in Russia, Bloomberg reported Monday.',
+    status: 1,
+    twitter: '',
+    facebook: ''
+  },
+  // {
+  //   id: 'audi',
+  //   name: 'Audi',
+  //   description: '',
+  //   status: 0,
+  //   twitter: '',
+  //   facebook: ''
+  // },
+  // {
+  //   id: 'volkswagen',
+  //   name: 'Volkswagen',
+  //   description: '',
+  //   status: 0,
+  //   twitter: '',
+  //   facebook: ''
+  // },
+  // {
+  //   id: 'volvo',
+  //   name: 'Volvo',
+  //   description: '',
+  //   status: 0,
+  //   twitter: '',
+  //   facebook: ''
+  // }
+
+]
