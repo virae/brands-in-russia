79c2c60cab263735fb7f79d837ddf5114233641b
Michal Šimonfy <michal.simonfy@gmail.com>
2022-03-08 11:43:58 +0100

    Configure screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,28 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      '3xl': '1920px',
+      // => @media (min-width: 1536px) { ... }
+
+      '4xl': '2560px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
